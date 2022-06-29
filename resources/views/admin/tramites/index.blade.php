efdde494e408eaebe9fa0fa3c43a0b8315b1a525
korticux <luis_cortina18@hotmail.com>
2022-06-28 19:25:54 -0700

    Tramites crud

--- resources/views/admin/tramites/index.blade.php
+++ resources/views/admin/tramites/index.blade.php
@@ -1,5 +1,47 @@
 @extends('admin.admin_master')
 
 @section('admin')
-    <h1 class="text-danger">TODES LES TRAMITES</h1>
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Datatables</h5>
+                    <a class="btn btn-primary" href="{{ route('tramites.post') }}"> <i class="bi bi-plus-circle"></i> &nbsp;
+                        Agregar
+                        Tramite</a>
+
+                    <!-- Table with stripped rows -->
+                    <table class="table datatable">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Fecha Agregado</th>
+                                <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        @foreach ($tramites as $tramite)
+                            <tbody>
+                                <tr>
+                                    <th>{{ $tramite->nombre }}</th>
+                                    <th>{{ $tramite->created_at }}</th>
+                                    <th class="row">
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <a class="btn  btn-sm btn-outline-dark" href="{{ route('tramites.edit', $tramite->id) }}" ><i class="bi bi-pencil-fill"></i></a>
+                                            <a class="btn btn-sm btn-outline-dark" id="delete"
+                                                href="{{ route('tramites.delete', $tramite->id) }}"><i
+                                                    class="bi bi-trash-fill"></i></a>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        @endforeach
+                    </table>
+                    <!-- End Table with stripped rows -->
+
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
