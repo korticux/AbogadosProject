b855eb182f741782d41b80063b9d82303d461e97
Lekaba <lct_jorge@hotmail.comm>
2022-06-29 16:23:50 -0700

    Bootstrap icons

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -21,63 +21,63 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('actores.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-people"></i>
                 <span>Actores</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('cuentas.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-bank"></i>
                 <span>Cuentas</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('dependencias.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-building"></i>
                 <span>Dependencias</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('estatus.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-clipboard-data"></i>
                 <span>Estatus</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('expedientes.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-briefcase"></i>
                 <span>Expedientes</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('festivos.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-pin-angle"></i>
                 <span>Festivos</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('municipios.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-pin-map"></i>
                 <span>Municipios</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('notificaciones.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-bell"></i>
                 <span>Notificaciones</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('cobranza.index') }}">
-                <i class="bi bi-map"></i>
+                <i class="bi bi-coin"></i>
                 <span>Cobranza</span>
             </a>
         </li><!-- End Blank Page Nav -->
@@ -91,14 +91,14 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('peticiones.index') }}">
-                <i class="bi bi-brush"></i>
+                <i class="bi bi-inbox"></i>
                 <span>Peticiones</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('proceso.index') }}">
-                <i class="bi bi-brush"></i>
+                <i class="bi bi-hourglass"></i>
                 <span>Proceso</span>
             </a>
         </li><!-- End Blank Page Nav -->
@@ -106,14 +106,14 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('regiones.index') }}">
-                <i class="bi bi-brush"></i>
+                <i class="bi bi-compass"></i>
                 <span>Regiones</span>
             </a>
         </li><!-- End Blank Page Nav -->
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('respaldo.index') }}">
-                <i class="bi bi-brush"></i>
+                <i class="bi bi-cloud-arrow-up"></i>
                 <span>Respaldo</span>
             </a>
         </li><!-- End Blank Page Nav -->
@@ -128,7 +128,7 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('tramites.index') }}">
-                <i class="bi bi-brush"></i>
+                <i class="bi bi-clipboard"></i>
                 <span>Tramites</span>
             </a>
         </li><!-- End Blank Page Nav -->
