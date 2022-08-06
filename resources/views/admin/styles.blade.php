630f840461338d2728825779c32854a2f9294866
korticux <luis_cortina18@hotmail.com>
2022-08-05 20:28:48 -0700

    headers

--- resources/views/admin/styles.blade.php
+++ resources/views/admin/styles.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <title>Log in | Abogados Diaz</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
