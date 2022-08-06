6a6f54d926473b71b2264f10dce692c2220b93f3
Lekaba <lct_jorge@hotmail.comm>
2022-08-05 22:19:33 -0700

    logos

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="dashboard" class="logo d-flex align-items-center">
             <img src="{{ asset('backend/assets/img/logo.png') }}" alt="">
             <span class="d-none d-lg-block">Abogados Díaz</span>
         </a>
@@ -9,6 +9,14 @@
     </div><!-- End Logo -->
 
     <nav class="header-nav ms-auto">
+        @php
+            use App\Models\User;
+            $users = User::all();
+            foreach ($users as $user)
+                $user->name
+
+        @endphp
+
         <ul class="d-flex align-items-center">
 
             <li class="nav-item dropdown">
@@ -18,7 +26,7 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <span class="d-none d-md-block dropdown-toggle ps-2">Username</span>
+                    <span class="d-none d-md-block dropdown-toggle ps-2">{{$user->email}}</span>
                 </a><!-- End Profile Iamge Icon -->
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
