40f03f6c917654e3fe886247c90a6bed1017a31e
korticux <luis_cortina18@hotmail.com>
2022-08-15 20:48:07 -0700

    Migartion

---
+++ database/migrations/2022_08_16_031832_create_pagos_cobranza_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos_cobranza', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string('nombre_pago');
+            $table->unsignedBigInteger('cobranza_id')->nullable();
+            $table->foreign('cobranza_id')->references('id')->on('cobranzas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos_cobranza');
+    }
+};
---
+++ database/migrations/2022_08_16_034545_add_properties_to_actores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actores', function (Blueprint $table) {
+            $table->string("honorario")->nullable();
+            $table->date("fecha1")->nullable();
+            $table->string("Abonado")->nullable();
+            $table->unsignedBigInteger("dependencia_id")->nullable();
+            $table->foreign("dependencia_id")->references('id')->on('dependencias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actores', function (Blueprint $table) {
+            //
+        });
+    }
+};
