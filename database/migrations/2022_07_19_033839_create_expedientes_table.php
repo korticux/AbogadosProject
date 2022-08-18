58913c798cea8268c23b492da0dea81c0511b7ae
korticux <luis_cortina18@hotmail.com>
2022-08-17 19:56:39 -0700

    korti

--- database/migrations/2022_07_19_033839_create_expedientes_table.php
+++ database/migrations/2022_07_19_033839_create_expedientes_table.php
@@ -43,30 +43,7 @@ return new class extends Migration
             $table->date("fecha5")->nullable();
             $table->date("fecha6")->nullable();
             $table->date("fecha7")->nullable();
-            $table->date("fecha8")->nullable();
-            $table->date("fecha9")->nullable();
-            $table->date("fecha10")->nullable();
-            $table->date("fecha11")->nullable();
-            $table->date("fecha12")->nullable();
-            $table->date("fecha13")->nullable();
-            $table->date("fecha14")->nullable();
-            $table->date("fecha15")->nullable();
-            $table->string("comentario1")->nullable();
-            $table->string("comentario2")->nullable();
-            $table->string("comentario3")->nullable();
-            $table->string("comentario4")->nullable();
-            $table->string("comentario5")->nullable();
-            $table->string("comentario6")->nullable();
-            $table->string("comentario7")->nullable();
-            $table->string("comentario8")->nullable();
-            $table->string("comentario9")->nullable();
-            $table->string("comentario10")->nullable();
-            $table->string("comentario12")->nullable();
-            $table->string("comentario13")->nullable();
-            $table->string("comentario14")->nullable();
-            $table->string("comentario15")->nullable();
-            $table->string("tipo_actor")->nullable();
-            $table->string("numero_exp")->nullable();
+
             $table->timestamps();
         });
     }
